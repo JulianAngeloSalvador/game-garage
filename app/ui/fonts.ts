4f7cdb7d1063da72edd13af3e4d28244c388c0ff
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-07-30 11:48:21 +0800

    feat(ui/fonts): removed unused fonts + transitioned main font to dm_sans

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,14 +1,6 @@
-import { Inter, Jersey_10, Press_Start_2P, Ubuntu } from "next/font/google";
+import { DM_Sans, Inter, Jersey_10 } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-
-const start_2p = Press_Start_2P({
-  preload: true,
-  display: "swap",
-  weight: ["400"],
-  subsets: ["latin"],
-  variable: "--font-start-2p",
-});
 
 const jersey_10 = Jersey_10({
   preload: true,
@@ -18,12 +10,12 @@ const jersey_10 = Jersey_10({
   variable: "--font-jersey-10",
 });
 
-const ubuntu = Ubuntu({
+const dm_sans = DM_Sans({
   preload: true,
   display: "swap",
   weight: ["300", "400", "500", "700"],
   subsets: ["latin"],
-  variable: "--font-ubuntu",
+  variable: "--font-dm-sans",
 });
 
-export { inter, start_2p, jersey_10, ubuntu };
+export { inter, jersey_10, dm_sans };
