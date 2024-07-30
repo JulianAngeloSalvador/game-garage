286c4ac625d8560aeb560d115216f89fd65cd6a2
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-07-30 11:49:45 +0800

    style: utilized tw's plugin to implement txtshdw + added custom boxshdw + default transition durs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   content: [
@@ -14,11 +15,16 @@ const config: Config = {
       extrabold: "700",
     },
     fontSize: {
-      "special-headline": "clamp(6rem, 10vw + 1rem, 10rem)",
+      "special-headline": "clamp(7rem, 16vw + 1rem, 14rem)",
       headline: "clamp(3rem, 5vw + 1rem, 5rem)",
-      subheadline: "clamp(2.25rem, 3vw + 1rem, 3rem)",
+      subheadline: "clamp(2rem, 3vw + 1rem, 3rem)",
       title: "clamp(1.85rem, 2vw + 1rem, 2.15rem)",
-      regular: "clamp(.94rem, 1.5vw + 1rem, 1rem)",
+      regular: "clamp(1rem, 1.5vw + 1rem, 1.215rem)",
+    },
+    transitionDuration: {
+      fast: ".25s",
+      medium: ".5s",
+      slow: ".75s",
     },
     extend: {
       backgroundImage: {
@@ -28,17 +34,46 @@ const config: Config = {
       },
       colors: {
         primary: "#020617",
+        "primary-light": " #030a25",
+        "primary-dark": " #010209",
+        "secondary-dark": "#595959",
         "secondary-lighter": "#FAFAFA",
+        white: "#ffffff",
         secondary: "#DFDFDF",
         faded: "#BFBFBF",
       },
       fontFamily: {
-        "start-2p": ["var(--font-start-2p)"],
         "jersey-10": ["var(--font-jersey-10)"],
-        ubuntu: ["var(--font-ubuntu)"],
+        "dm-sans": ["var(--font-dm-sans)"],
+      },
+      boxShadow: {
+        "primary-neumorphy-outer":
+          "5px 5px 10px theme(colors.primary-dark), -5px -5px 10px theme(colors.primary-light)",
+        "primary-neumorphy-inner":
+          "inset 5px 5px 10px theme(colors.primary-dark), inset -5px -5px 10px theme(colors.primary-light)",
+        "secondary-neumorphy-outer":
+          "5px 5px 10px theme(colors.secondary-dark), -5px -5px 10px theme(colors.white)",
+        "secondary-neumorphy-inner":
+          "inset 5px 5px 10px theme(colors.secondary-dark), inset -5px -5px 10px theme(colors.white)",
+      },
+      textShadow: {
+        sm: "0 1px 2px var(--tw-shadow-color)",
+        DEFAULT: "0 2px 4px var(--tw-shadow-color)",
+        lg: "0 8px 16px var(--tw-shadow-color)",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "text-shadow": (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme("textShadow") }
+      );
+    }),
+  ],
 };
 export default config;
