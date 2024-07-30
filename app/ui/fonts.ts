3d62e7dc07a359a85182364079f9890816ee82ec
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-07-30 10:01:41 +0800

    feat(ui/fonts): added app fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,29 @@
-import { Inter } from "next/font/google";
+import { Inter, Jersey_10, Press_Start_2P, Ubuntu } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export { inter };
+const start_2p = Press_Start_2P({
+  preload: true,
+  display: "swap",
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-start-2p",
+});
+
+const jersey_10 = Jersey_10({
+  preload: true,
+  display: "swap",
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-jersey-10",
+});
+
+const ubuntu = Ubuntu({
+  preload: true,
+  display: "swap",
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  variable: "--font-ubuntu",
+});
+
+export { inter, start_2p, jersey_10, ubuntu };
