37993a949fcd10b02ea423fd9a8882e0a87cf2af
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-07-30 11:51:33 +0800

    feat(homepage): basic landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import { content } from "./contents/landing/landingContent";
+import { capitalize, capitalizeEach } from "./lib/utils";
+import CustomButton from "./ui/components/CustomButton";
 
 export default function Home() {
   return (
     <main>
-      <section>
-        <header>
-          <h1>{content.headline}</h1>
-          <h3>{content.subheadline}</h3>
+      <section className="min-h-dvh grid place-content-center gap-y-14">
+        <header className="px-6 text-center text-balance">
+          <h1>{capitalizeEach(content.headline as string)}</h1>
+          <h3 className="mt-6">{capitalize(content.subheadline as string)}</h3>
         </header>
+        <CustomButton className="justify-self-center text-title font-jersey-10 tracking-wider">
+          {capitalizeEach(content.cta as string)}
+        </CustomButton>
       </section>
     </main>
   );
