7fee10ff628dcd6cee7976c663703272ba4e475f
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-07-30 10:05:35 +0800

    feat(homepage): added basic landing content

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import { content } from "./contents/landing/landingContent";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <section>
+        <header>
+          <h1>{content.headline}</h1>
+          <h3>{content.subheadline}</h3>
+        </header>
+      </section>
+    </main>
+  );
 }
