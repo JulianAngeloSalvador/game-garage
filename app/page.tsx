66fb63d9d5ca84fbdff84b0ce82c6fc0a9a2b734
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-08-05 20:30:17 +0800

    style(homepage): restyled homepage elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,30 @@
 import { content } from "./contents/landing/landingContent";
 import { capitalize, capitalizeEach } from "./lib/utils";
-import CustomButton from "./ui/components/CustomButton";
+import styles from "./ui/styles/custom.module.css";
 
 export default function Home() {
   return (
     <main>
       <section className="min-h-dvh grid place-content-center gap-y-14">
-        <header className="px-6 text-center text-balance">
+        <header className="px-6 text-center text-balance mb-10">
           <h1>{capitalizeEach(content.headline as string)}</h1>
-          <h3 className="mt-6">{capitalize(content.subheadline as string)}</h3>
+          <h3 className="mt-6 font-bungee">
+            {capitalize(content.subheadline as string)}
+          </h3>
         </header>
-        <CustomButton className="justify-self-center text-title font-jersey-10 tracking-wider">
-          {capitalizeEach(content.cta as string)}
-        </CustomButton>
+
+        <div
+          className={`justify-self-center grid place-content-center relative ${styles.perspective1000}`}
+        >
+          <button
+            className={`px-4 py-2 bg-gradient-to-b from-secondary-lighter to-secondary text-secondary-dark text-shadow-lg shadow-secondary-dark font-extrabold  outline-none  hover:text-teal-500 hover:text-shadow-leaning hover:shadow-green-300 group ${styles.rotateFront} `}
+          >
+            {capitalizeEach(content.cta as string)}
+          </button>
+          <div
+            className={`absolute top-2 outline outline-2 w-full h-full -z-10 outline-secondary ${styles.rotateFront}`}
+          />
+        </div>
       </section>
     </main>
   );
